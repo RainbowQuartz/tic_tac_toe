6bc666839a1eab6ce303281e48870b3b86f406e7
Léona Chevrel <chevrel.leona@gmail.com>
2018-07-17 13:32:45 +0200

    modification of the array Boardcases

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,26 @@
 require 'pry'
 
 class Game
+  @@turns = 1
+
   def initialize
     Players.new
     Board.new
+  end
+
+  def Game.turn
+    if @@turns.odd?
+    print "#{$player_1}'s turn, enter the row in which you want to play"
+    row = gets.to_i
+    print "enter the column in which you want to play"
+    column = gets.to_i
+    else
+    print "#{$player_2}'s turn, enter the row in which you want to play"
+    row = gets.to_i
+    print "enter the column in which you want to play"
+    column = gets.to_i
+    end
+    @@turns += 1
   end
 end
 
@@ -14,30 +31,21 @@ class Board < Game
 end
 
 class Players < Game
+  attr_accessor :player_1, :player_2
+
   def initialize
     p 'Name of player 1 :'
     print '>'
-    @player_1 = gets.chomp
+    $player_1 = gets.chomp
     p 'Name of player 2 :'
     print '>'
-    @player_2 = gets.chomp
+    $player_2 = gets.chomp
   end
 end
 
 class Boardcases < Board
   def initialize
-    @case_1 = '|_|'
-    @case_2 = '|_|'
-    @case_3 = '|_|'
-    @case_4 = '|_|'
-    @case_5 = '|_|'
-    @case_6 = '|_|'
-    @case_7 = '|_|'
-    @case_8 = '|_|'
-    @case_9 = '|_|'
-    puts @case_1 + @case_2 + @case_3
-    puts @case_4 + @case_5 + @case_6
-    puts @case_7 + @case_8 + @case_9
+    $Boardcases =  Array.new(3) { Array.new(3, '|_|')}
   end
 end
 
